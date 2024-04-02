7809c33424a60f20b6049b9987eef370fe7ee512
laihaoqiang <laihaoqiang34@gmail.com>
2024-04-02 10:45:07 +1000

    clear firebase

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import DeleteForeverOutlinedIcon from "@mui/icons-material/DeleteForeverOutlined";
 import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 import HighlightOffIcon from "@mui/icons-material/HighlightOff";
